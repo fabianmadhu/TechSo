7a37ba21b13512f9c2c4055967f40575b1ff93f7
fabianMadhu <fabianmadhu@gmail.com>
2024-05-12 08:24:53 +0530

    Initial commit

---
+++ application/views/includes/footer.php
@@ -0,0 +1,11 @@
+</div>
+
+<footer class="footer">
+    <div class="container">
+        <p class="text-muted text-center"><a target="_blank" href="https://www.linkedin.com/in/fabian-madhushan-rasanayagam-8b52891b9/">About us</a> | <a target="_blank" href="https://www.linkedin.com/in/fabian-madhushan-rasanayagam-8b52891b9/"> Contact us</a>
+        </p>
+    </div>
+</footer>
+</body>
+
+</html>
